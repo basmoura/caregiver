2c16fbe04d133689cfc4d4a91c266a5b4064d59a
Breno Moura <basmoura@gmail.com>
2025-05-16 14:39:29 -0300

    fix: rubocop auto-fix

--- db/migrate/20250516140134_add_unique_index_on_agency.rb
+++ db/migrate/20250516140134_add_unique_index_on_agency.rb
@@ -1,6 +1,6 @@
 class AddUniqueIndexOnAgency < ActiveRecord::Migration[8.0]
   def change
-    add_index :agencies, [:name, :address], unique: true
+    add_index :agencies, [ :name, :address ], unique: true
     add_index :agencies, :name
     add_index :agencies, :address
   end
